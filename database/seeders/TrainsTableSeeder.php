70a18ea24b1543e78cd375d67d1b2f32b2253921
peppeluc22 <gluca0260@gmail.com>
2023-05-24 16:40:29 +0200

    populated  table

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $trains = config('trains', 'trains');
+        foreach ($trains as $train) {
+            $newTrain = new Train();
+            $newTrain->agency = $train['agency'];
+            $newTrain->departure_station = $train['departure_station'];
+            $newTrain->arrival_station = $train['arrival_station'];
+            $newTrain->departure_time = $train['departure_time'];
+            $newTrain->arrival_time = $train['arrival_time'];
+            $newTrain->departure_time = $train['departure_time'];
+            $newTrain->train_code = $train['train_code'];
+            $newTrain->coach_number = $train['coach_number'];
+            $newTrain->in_time = $train['in_time'];
+            $newTrain->deleted = $train['deleted'];
+            $newTrain->save();
+        }
+    }
+}
