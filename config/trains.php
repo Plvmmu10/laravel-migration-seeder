9eb93276f16d81d7cde251308ae9ebb68c1bef79
peppeluc22 <gluca0260@gmail.com>
2023-05-24 16:38:52 +0200

    created trains db array

---
+++ config/trains.php
@@ -0,0 +1,61 @@
+<?php
+
+return
+    $trains = [
+        [
+            "agency" => "Trenitalia",
+            "departure_station" => "Trapani",
+            "arrival_station" => "Messina",
+            "departure_time" => "19:00",
+            "arrival_time" => "22:00",
+            "train_code" => "FGK283",
+            "coach_number" => 7,
+            "in_time" => true,
+            "deleted" => false
+
+        ],
+        [
+            "agency" => "Italo",
+            "departure_station" => "Milano",
+            "arrival_station" => "Roma",
+            "departure_time" => "12:00",
+            "arrival_time" => "15:00",
+            "train_code" => "DTY239",
+            "coach_number" => 10,
+            "in_time" => true,
+            "deleted" => false
+        ],
+        [
+            "agency" => "Trenitalia",
+            "departure_station" => "Napoli",
+            "arrival_station" => "Palermo",
+            "departure_time" => "11:00",
+            "arrival_time" => "20:00",
+            "train_code" => "FDS263",
+            "coach_number" => 5,
+            "in_time" => false,
+            "deleted" => true
+        ],
+        [
+            "agency" => "Trenitalia",
+            "departure_station" => "Messina",
+            "arrival_station" => "Bari",
+            "departure_time" => "18:00",
+            "arrival_time" => "01:00",
+            "train_code" => "PLK781",
+            "coach_number" => 9,
+            "in_time" => true,
+            "deleted" => false
+        ],
+        [
+            "agency" => "Italo",
+            "departure_station" => "Firenze",
+            "arrival_station" => "Civitavecchia",
+            "departure_time" => "06:00",
+            "arrival_time" => "08:00",
+            "train_code" => "LSG392",
+            "coach_number" => 10,
+            "in_time" => true,
+            "deleted" => false
+        ]
+    ];
